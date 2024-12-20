fee4ae0811a42557829774f01864f8dbc4bf9676
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-20 15:53:55 +0300

    write tests for WorkerInTeamListAPIView

--- src/workers/tests.py
+++ src/workers/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from workers.models import Worker
+
+
+class WorkerTestCase(APITestCase):
+    def setUp(self):
+        self.worker1 = Worker.objects.create(
+            full_name="Тест 1",
+            team_id=1,
+            salary=10000,
+            specialization="1"
+        )
+        self.worker2 = Worker.objects.create(
+            full_name="Тест 2",
+            team_id=2,
+            salary=10000,
+            specialization="2"
+        )
+        self.worker3 = Worker.objects.create(
+            full_name="Тест 3",
+            team_id=1,
+            salary=12000
+        )
+
+    def test_worker_list(self):
+        url = reverse("v1:worker-list", args=(self.worker1.team_id,))
+        response = self.client.get(url)
+        data = response.json()
+        result = {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "full_name": "Тест 1",
+                    "team_id": 1,
+                    "salary": 10000,
+                    "specialization": "1"
+                },
+                {
+                    "id": 3,
+                    "full_name": "Тест 3",
+                    "team_id": 1,
+                    "salary": 12000,
+                    "specialization": "3"
+                }
+            ]
+        }
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
