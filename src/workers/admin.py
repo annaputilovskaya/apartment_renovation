c8f4b9bd9fbf6259d02991e5745b0977237e1c39
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-20 09:49:11 +0300

    create WorkerAdmin

--- src/workers/admin.py
+++ src/workers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from workers.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "team_id", "salary", "specialization")
+    list_filter = ("team_id", "specialization")
+    ordering = ("full_name",)
