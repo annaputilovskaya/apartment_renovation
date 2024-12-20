630f021517cdd4e080f4016489c8c2542f223db5
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-20 10:49:47 +0300

    add worker-detail to urlpatterns

--- src/api/v1/workers/urls.py
+++ src/api/v1/workers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.v1.workers.views import WorkerInTeamList
+from api.v1.workers.views import WorkerInTeamListAPIView, WorkerDetailAPIView
 
 urlpatterns = [
-    path('team/<int:team_id>/WorkerList/', WorkerInTeamList.as_view(), name='worker-list'),
-    ]
+    path('team/<int:team_id>/WorkerList/', WorkerInTeamListAPIView.as_view(), name='worker-list'),
+    path('worker/<int:pk>/', WorkerDetailAPIView.as_view(), name='worker-detail'),
+]
