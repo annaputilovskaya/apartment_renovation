c13a9d7bc63e39b29fabf84a4b1d3e6fa8bf4c92
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-20 09:42:42 +0300

    create model Worker

--- src/workers/models.py
+++ src/workers/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Worker(models.Model):
+    """
+    Модель рабочего.
+    """
+
+    SPECIALIZATION_CHOICES = [
+        ("1", "Черновая отделка"),
+        ("2", "Чистовая отделка"),
+        ("3", "Бригадир"),
+        ("4", "Прораб")
+    ]
+
+    full_name = models.CharField(max_length=100, verbose_name="ФИО")
+    team_id = models.PositiveSmallIntegerField(**NULLABLE, verbose_name="Номер бригады")
+    salary = models.PositiveIntegerField(verbose_name="Заработная плата")
+    specialization = models.CharField(
+        max_length=100, choices=SPECIALIZATION_CHOICES, default="3", verbose_name="Специализация"
+    )
+
+    class Meta:
+        verbose_name = "Рабочий"
+        verbose_name_plural = "Рабочие"
+        ordering = ["full_name"]
+
+    def __str__(self):
+        return self.full_name
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}"
+                f"({self.full_name=}, {self.team_id=}, {self.salary=}, {self.specialization=})")
